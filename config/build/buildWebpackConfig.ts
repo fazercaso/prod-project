c013ecfc7a01a233084a0b44a3a026e4407ca7a0
Максимилиан Андриянов <fazercaso@gmail.com>
2023-02-13 10:25:44 +0300

    first

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,37 @@
+import path from "path";
+import webpack from "webpack";
+import { buildDevServer } from "./buildDevServer";
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buildResolvers } from "./buildResolvers";
+import { BuildOptions } from "./types/config";
+
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const { paths, mode, isDev } = options;
+    return {
+        //MODE - ВЫБИРАЕМ ПРОДАКШЕМ ИЛИ ДЕВЕЛОПМЕНТ
+        mode: mode,
+        // ТОЧКА ВХОДА - СТРОКОЙ ИЛИ ОБЪЕКТОМ, ЕСЛИ НАМ НУЖНО НЕСКОЛЬКО
+        // В БАНДЛЕ СОЗДАДУТСЯ ФАЙЛЫ JS С ТОЧКАМИ ВХОДА
+        entry: paths.entry,
+        // ВЫХОД - УКАЗЫВАЕТСЯ ФАЙЛНЕЙМ, В КВАДРАТНЫХ СКОБКАХ ЕСЛИ ДИНАМИЧЕСКОЕ НАЗВАНИЕ
+        // contenthash УДАЛЯЕТ ФАЙЛЫ ПРЕДЫДУЩИХ СБОРОКб РЕШАЕТ ПРОБЛЕМУ КЭША
+        output: {
+            filename: '[name].[contenthash].js',
+            path: paths.build,
+            clean: true,
+        },
+        module: {
+            //САМОЕ ВАЖНОЕ
+            //ЛЮБАЯ ОБРАБОТКА ФАЙЛОМ ВЫХОДЯЩИЕ ЗА ПРЕДЕЛЫ ЖС
+            //В НАШЕ СЛУЧАЕ ФАЙЛЫ TS И TSX
+            rules: buildLoaders(options),
+        },
+          //RESOLVE ОТВЕЧАЕТ ЗА ИМПОРТ КОМПОНЕНТОВ БЕЗ УКАЗАНИЯ РАСШИРЕНИЯ ФАЙЛА
+          resolve: buildResolvers(),
+        plugins: buildPlugins(options),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+        
+      }
+}
