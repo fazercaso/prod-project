1c912741540f636149ed819390cb7d634197c759
Максимилиан Андриянов <fazercaso@gmail.com>
2023-03-14 21:13:55 +0300

    add redux store, add first entity

---
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -0,0 +1,29 @@
+import { counterReducer } from 'entities/Counter';
+import { CounterSchema } from 'entities/Counter/model/types/counterSchema';
+import { counterActions } from './counterSlice';
+
+describe('counterSlice.test', () => {
+    test('decrement', () => {
+        const state: CounterSchema = { value: 10 };
+
+        expect(
+            counterReducer(state, counterActions.decrement()),
+        )
+            .toEqual({ value: 9 });
+    });
+    test('increment', () => {
+        const state: CounterSchema = { value: 10 };
+
+        expect(
+            counterReducer(state, counterActions.increment()),
+        )
+            .toEqual({ value: 11 });
+    });
+
+    test('should work with empty state', () => {
+        expect(
+            counterReducer(undefined, counterActions.increment()),
+        )
+            .toEqual({ value: 1 });
+    });
+});
---
+++ src/entities/Counter/model/types/counterSchema.ts
@@ -0,0 +1,4 @@
+export interface CounterSchema {
+    [x: string]: any;
+    value: number
+}
